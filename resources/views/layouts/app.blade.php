ea090e741d27e4ac43dbfa02bcf4ff189ee6667a
Wallace F <ideiacompany@gmail.com>
2025-05-16 11:41:44 -0400

    fix: adding links in view

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,20 +12,42 @@
     <header class="bg-blue-700 text-white p-4 shadow-md flex items-center justify-between">
         <h1 class="text-xl font-bold">Biblioteca Gestor App Fácil</h1>
         <nav class="space-x-4 hidden md:block">
-            <a href="#" class="hover:underline">Home</a>
-            <a href="#" class="hover:underline">Usuários</a>
-            <a href="#" class="hover:underline">Livros</a>
-            <a href="#" class="hover:underline">Empréstimos</a>
+            <a href="{{ url('/') }}" class="hover:underline">
+                Home
+            </a>
+            <a href="{{ route('users.index') }}" class="hover:underline">
+                Usuários
+            </a>
+            <a href="{{ route('genres.index') }}" class="hover:underline">
+                Gêneros
+            </a>
+            <a href="{{ route('books.index') }}" class="hover:underline">
+                Livros
+            </a>
+            <a href="{{ route('loans.index') }}" class="hover:underline">
+                Empréstimos
+            </a>
         </nav>
     </header>
 
     <div class="flex flex-1">
         <aside class="bg-white w-64 p-6 border-r border-gray-200 hidden md:block">
             <nav class="flex flex-col space-y-4">
-                <a href="#" class="text-gray-700 hover:text-blue-700 font-semibold">Home</a>
-                <a href="#" class="text-gray-700 hover:text-blue-700 font-semibold">Usuários</a>
-                <a href="#" class="text-gray-700 hover:text-blue-700 font-semibold">Livros</a>
-                <a href="#" class="text-gray-700 hover:text-blue-700 font-semibold">Empréstimos</a>
+                <a href="{{ url('/') }}" class="text-gray-700 hover:text-blue-700 font-semibold">
+                    Home
+                </a>
+                <a href="{{ route('users.index') }}" class="text-gray-700 hover:text-blue-700 font-semibold">
+                    Usuários
+                </a>
+                <a href="{{ route('genres.index') }}" class="text-gray-700 hover:text-blue-700 font-semibold">
+                    Gêneros
+                </a>
+                <a href="{{ route('books.index') }}" class="text-gray-700 hover:text-blue-700 font-semibold">
+                    Livros
+                </a>
+                <a href="{{ route('loans.index') }}" class="text-gray-700 hover:text-blue-700 font-semibold">
+                    Empréstimos
+                </a>
             </nav>
         </aside>
         <main class="flex-1 p-6">
